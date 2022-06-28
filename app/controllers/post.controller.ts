ce42a4b4f95eee31fdcf757f226fb25315800cf9
beckerkadji <kadjibecker@gmail.com>
2022-06-28 18:07:06 +0100

    perform upload with cloudinary signature

--- app/controllers/post.controller.ts
+++ app/controllers/post.controller.ts
@@ -4,6 +4,7 @@ import { ResponseHandler } from "../../src/config/responseHandler";
 import code from "../../src/config/code";
 import PostType from "../types/postType";
 import { postSchema } from "../validations/post.validation";
+import {PostModel} from "../models/post"
 const response = new ResponseHandler()
 
 @Tags("Post Controller")
@@ -15,17 +16,25 @@ export class postController extends My_Controller {
     public async create(
         @FormField() title : string,
         @FormField() description : string,
-        @UploadedFile() image : any
+        @UploadedFile() image : Express.Multer.File
     ): Promise<IResponse> {
         try {
         //    const validate =  this.validate(postSchema, body)
         //    if(validate !== true)
         //        return response.liteResponse(code.VALIDATION_ERROR, 'Validation error', validate)
-        let uploaded = await this.uploadFile(image)
-       
+        let url = await this.uploadFile(image)
+        console.log(url)
+
+        // const savePost =  await PostModel.create({data:{
+        //     userId: 34,
+        //     title : title,
+        //     description: description,
+        //     Images : uploaded.secure_url
+        // }})
+        
             
             
-            return response.liteResponse(code.SUCCESS, "User created with success !", )
+            return response.liteResponse(code.SUCCESS, "User created with success !", url)
         }catch(e){
             return response.catchHandler(e)
         }
